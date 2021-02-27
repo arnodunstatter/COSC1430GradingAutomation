2fc208af719050f9c7a1b9167ca3f6f516eeab98
Arno Dunstatter <61071417+arnodunstatter@users.noreply.github.com>
2021-02-26 19:19:38 -0600

    Delete Lab Attendance Grader directory

--- Lab Attendance Grader/previous versions/Old version without roster/Student.cpp
+++
@@ -1,6 +0,0 @@
-#include "Student.h"
-Student::Student(string name) //name is in format "last,first"
-{
-	fName = name.substr(name.find(',')+1);
-	lName = name.substr(0, name.length() - fName.length() - 1);
-}
